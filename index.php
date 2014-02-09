b2737c3b8ff25e3592a89b4333b401ccca93e185
amitoj <amitoj@singh.(none)>
2014-02-09 15:18:23 +0530

    work

---
+++ index.php
@@ -0,0 +1,34 @@
+<HTML>
+  <Head>
+    <title>Home</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="index.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.scss">
+  </head>
+      <body>
+	<header>
+                  <button class="toggle-nav">
+                            <span><img src="pencil1.png"></span>
+                  </button>
+                  <ul class="nav">
+                            <li class="loud"><a href="#">About</a></li>
+                            <li class="loud"><a href="#">fonts</a></li>
+                            <li class="loud"><a href="#">Submision</a></li>
+                            <li class="loud"><a href="#">Blog</a></li>
+                            <li class="loud"><a href="#">Contact</a></li>
+                  </ul>
+        </header>
+
+         <h1>ABCD Fonts</h1>
+          <p>Any Body Can Download</p>
+	  <a href="fonts.php">f</a>
+	
+	<div id="content">
+hello
+
+	</div>
+    </body> 
+</HTML>
+
+	
